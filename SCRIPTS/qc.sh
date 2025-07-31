c58e08b5de67c6d18835cd994b7fa4f8d626ad32
Calum Walsh <cazzlewazzle89@gmail.com>
2025-08-01 09:02:10 +1000

    updated qc

---
+++ SCRIPTS/qc.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+source method.man
+source "$CONDA_SH_PATH"
+
+# limit threads to 16 if running fastp
+if [[ "$QUALITY_TOOL" == "fastp" ]]
+then
+
+    if [[ "$THREADS" -gt 16 ]]
+    then
+
+        FASTP_THREADS=16
+
+    else
+
+        FASTP_THREADS="$THREADS"
+
+    fi
+
+fi
+
+# activate conda environment
+conda activate "$CONDA_ENV_QC"
+
+# make output directory
+mkdir -p "$OUTDIR"/FASTQ
+
+# run qc
+while read -r i j k 
+do
+
+    if [ "$QUALITY_TOOL" == "fastp" ]
+    then
+        
+        fastp \
+            --in1 "$j" \
+            --in2 "$k" \
+            --out1 "$OUTDIR"/FASTQ/"$i"_R1_trimmed.fq.gz \
+            --out2 "$OUTDIR"/FASTQ/"$i"_R2_trimmed.fq.gz \
+            --detect_adapter_for_pe \
+            --length_required 50 \
+            --thread "$FASTP_THREADS" \
+            --html "$OUTDIR"/FASTQ/"$i"_fastp.html \
+            --json "$OUTDIR"/FASTQ/"$i"_fastp.json
+
+    else
+
+        trim_galore \
+            --fastqc \
+            --length 50 \
+            -o "$OUTDIR"/FASTQ/ \
+            --basename "$i" \
+            --cores 1 \
+            --paired \
+            "$j" "$k"
+            
+    fi 
+
+done < "$MANIFEST"
+
--- wutang.sh
+++ wutang.sh
@@ -14,23 +14,82 @@ else
 fi
 
 # source conda
-if [[ -f "${CONDA_SH_PATH}" ]]
+if [[ -f "$CONDA_SH_PATH" ]]
 then
 
-    source "${CONDA_SH_PATH}"
+  source "$CONDA_SH_PATH"
 
 else
 
-    echo "ERROR: Cannot find conda.sh at ${CONDA_SH_PATH}"
+  echo "ERROR: Cannot find conda.sh at ${CONDA_SH_PATH}"
+  exit 1
+
+fi
+
+# confirm that manifest exists and all read files exist
+if [ ! -f "$MANIFEST" ]
+then
+
+  echo "Manifest file not found: ${MANIFEST}"
+  exit 1
+
+  else
+
+    inputerror="false"
+
+    while read -r i j k
+    do
+    
+      if [ ! -f "$j" ]
+      then
+		  
+        echo "File not found: ${j}"
+        inputerror="true"
+      
+      fi
+
+      if [ ! -f "$k" ]
+      then
+
+        echo "File not found: ${k}"
+        inputerror="true"
+      
+      fi
+
+  done < "$MANIFEST"
+
+  if [ "$inputerror" == "true" ]
+  then
+
+    echo "One or more input files are missing. Exiting."
     exit 1
+  
+  fi
+
+fi
+
+# confirm that specified qc tool is valid
+if [ "$QUALITY_TOOL" == 'fastp' ] || [ "$QUALITY_TOOL" == 'trimgalore' ]
+then
+
+  :
+
+else
+
+  echo 'qc tool not recognised'
+  exit 1
 
 fi
 
 # print configuration
-echo "Input: ${MANIFEST_FILE}"
-echo "Output: ${OUTPUT_DIR}"
+echo "Input: ${MANIFEST}"
+echo "Output: ${OUTDIR}"
 echo "Quality control: ${QUALITY_TOOL}"
 echo "Host removal: ${DECONTAM_TOOL}"
 
+# make output directory
+mkdir "$OUTDIR"
 
+# run qc
+bash SCRIPTS/qc.sh
 
